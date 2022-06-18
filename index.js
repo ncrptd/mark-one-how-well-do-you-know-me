8fb3fd24c9e67d22b9d58491131e672e9aae16e4
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-06-18 21:54:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var readlineSync = require('readline-sync');
 var score = 0;
+var userName = readlineSync.question('What is your name ? ')
+console.log('Welcome ' + userName + ' to how well do you know me quiz')
 function play(question, answer){
   var userAnswer = readlineSync.question(question);
   if(userAnswer === answer){
@@ -14,13 +16,25 @@ function play(question, answer){
 
 
 const questions = [{
-  question: 'What is my Name? ',
-  answer: 'Rockey'
+  question: 'What is my full Name ? ',
+  answer: 'Rockey Biswas'
 },
     {
   question: 'Where Do I Live? ',
   answer: 'Gurgaon'
-}              ]
+},
+    {
+  question: 'What is my favourite street food? ',
+  answer: 'Momo'
+},
+    {
+  question: 'What is my favourite movie? ',
+  answer: 'Shawshank Redemption'
+}    ,
+    {
+  question: 'How Old Am i ? ',
+  answer: '22'
+}            ]
 
 for(let i = 0; i<questions.length; i++){
   play(questions[i].question, questions[i].answer)
